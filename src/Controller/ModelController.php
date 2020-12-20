74b101640a599620ede6896a5a6688e4b134ac6f
qcalmel <quentin.calmel@gmail.com>
2020-12-20 21:00:20 +0100

    Add route in ModelController.php for import models from JSON file

--- src/Controller/ModelController.php
+++ src/Controller/ModelController.php
@@ -7,18 +7,55 @@ use App\Entity\Picture;
 use App\Form\ModelType;
 use App\Repository\ModelRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 /**
  * @Route("/model")
  */
 class ModelController extends AbstractController
 {
+    /**
+     * @Route("/json_add", name="model_json_add", methods={"GET","POST"})
+     */
+    public function addModelFromJSON(Request $request){
+        $form = $this->createFormBuilder(null)
+            ->add('json_file',FileType::class,[
+                'label'=>"Fichier JSON",
+            ])
+            ->add(('add'),SubmitType::class,[
+                'label'=>'Importer',
+                'attr'=>[
+                    'class'=>'btn btn-primary'
+                ],
+            ])
+            ->getForm();
+        dump($request);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $JSONData = file_get_contents($form->get('json_file')->getData());
+            $data = json_decode($JSONData,true);
+            dump($data);
+             $message = "fichier ok";
+        }
+
+            return $this->render('/model/add.html.twig', [
+                'form' => $form->createView(),
+                'message' => $message ?? null
+            ]);
+
+
+    }
+
     /**
      * @Route("/", name="model_index", methods={"GET"})
      */
@@ -186,4 +223,5 @@ class ModelController extends AbstractController
             ]);
         }
     }
+
 }
